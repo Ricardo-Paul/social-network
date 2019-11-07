c3793f07852bb701309871301457539f30798d5f
RickyyCoder <ricardopaul459@gmail.com>
2019-11-07 11:35:33 -0500

    setup firebase authentication - implememt user signup

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: "2019-11-07T15:57:20.173Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -2,40 +2,52 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-const express = require('express');
-const app = express();
-
+const app = require('express')();
 admin.initializeApp();
+
+
+// ---- Adding firebase to our app
+const config = {
+    apiKey: "AIzaSyCVDFsCccFAebluJ5O1s51A9QP3YknOVic",
+    authDomain: "socialape-eaef4.firebaseapp.com",
+    databaseURL: "https://socialape-eaef4.firebaseio.com",
+    projectId: "socialape-eaef4",
+    storageBucket: "socialape-eaef4.appspot.com",
+    messagingSenderId: "370403092046",
+    appId: "1:370403092046:web:7d91c05024d801bdc3c51f",
+    measurementId: "G-VX91V8X0T2"
+  };
+
+//   after installing firebase inside funcitons folder
+// npm install --save firebase
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
 
 app.get('/screams', (request, response) => {
     admin
     .firestore()
     .collection('screams')
+    .orderBy( 'createdAt', 'desc' )
     .get()
     .then( (data) => {
         let screams = [];
         data.forEach( (doc) => {
-            screams.push(doc.data())
+            screams.push(
+                // doc.data()
+                // reshape the output of the json using the doc id and data()
+                {
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                }
+            );
         });
         return response.json(screams);
     })
     .catch( (err) => console.error(err));
 })
-
-// exports.getScreams = functions.https.onRequest( (request, response) => {
-//     admin
-//     .firestore()
-//     .collection('screams')
-//     .get()
-//     .then( (data) => {
-//         let screams = [];
-//         data.forEach( (doc) => {
-//             screams.push(doc.data())
-//         });
-//         return response.json(screams);
-//     })
-//     .catch( (err) => console.error(err));
-// })
 
 app.post('/scream', (request, response) => {
     // if (request.method !== 'POST') {
@@ -44,7 +56,8 @@ app.post('/scream', (request, response) => {
     const newScream = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        // createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
@@ -62,5 +75,26 @@ app.post('/scream', (request, response) => {
 
 // we want to have this style
 // https://baseurl/api/...
+
+
+// SignUp route
+app.post( '/signup', (request, response) =>{
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        handle: request.body.handle
+    }
+
+// Now using firebase to authenticate user
+firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+        return response.status(201).json({ message: `user ${data.user.uid} signed up successfully` })
+    })
+    .catch( (err) =>{
+        console.error(err);
+        return response.status(500).json({error: 'an error has occured'})
+    })
+})
 
 exports.api = functions.https.onRequest(app);
