569f0a5c77a1852ada309271dd6467420fa274b6
RickyyCoder <ricardopaul459@gmail.com>
2019-11-07 15:14:42 -0500

    User login implemented

--- functions/index.js
+++ functions/index.js
@@ -83,7 +83,16 @@ app.post('/scream', (request, response) => {
 
 
 
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
 
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
 
 // SignUp route
 let token, userId;
@@ -94,6 +103,20 @@ app.post( '/signup', (request, response) =>{
         confirmPassword: request.body.confirmPassword,
         handle: request.body.handle
     }
+
+    let errors = {};
+    if(isEmpty(newUser.email)){
+        errors.email = 'Email must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email adress'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must watch';
+
+    if(Object.keys(errors).length > 0) return response.status(400).json(errors);
+    
 
     // validate
     db.doc(`/users/${newUser.handle}`).get()
@@ -134,5 +157,40 @@ app.post( '/signup', (request, response) =>{
         }
     });
 });
+
+
+
+
+
+
+// Login 
+app.post('/login', (request, response) =>{
+    const user = {
+        email: request.body.email,
+        password: request.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = "Must not be empty";
+    if(isEmpty(user.password)) errors.password = "Must not be empty";
+
+    if(Object.keys(errors).length > 0) return response.status(400).json(errors)
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then( data => {
+        return data.user.getIdToken();
+    })
+    .then(token =>{
+        return response.json({token})
+    })
+    .catch(err => {
+        console.error(err);
+        return response.status(500).json({ errror: err.code })
+    });
+});
+
+// Ensure you get the user token everytime they login
+// data (the promise) data.user.getIdToken
 
 exports.api = functions.https.onRequest(app);
